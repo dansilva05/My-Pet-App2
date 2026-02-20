59d39137c498075b1d6dcf83602a2a0ca41b3914
silva-danilo05 <20112943@setu.ie>
2026-02-20 00:06:37 +0000

    Add search function to input

--- public/script.js
+++ public/script.js
@@ -1,1 +1,14 @@
+// the same way it is in My Recipe Scrolls
+const search = document.getElementById("search"); // like your recipe page
+const cards = document.querySelectorAll(".ui.card"); // all pet cards
 
+if (search) {
+  search.addEventListener("input", () => {
+    const term = search.value.toLowerCase();
+
+    cards.forEach((card) => {
+      const text = card.innerText.toLowerCase();
+      card.style.display = text.includes(term) ? "" : "none";
+    });
+  });
+}
